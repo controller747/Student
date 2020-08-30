e62d92307867e6199aee0c19ea9123fd0eff1096
controller747 <70430521+controller747@users.noreply.github.com>
2020-08-30 03:12:35 +0300

    index.php

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<style>
+    .table {
+		width: 350px;
+		border-collapse: collapse;
+	}
+	.row {
+		width: 100%;
+		display: flex;
+	}
+	.cell {
+		text-align: center;
+		width: 30%;
+		border: 1px solid black;
+	}
+	
+	</style>
+	<table border=1>
+	    <tr>
+		    <th>Header1</th>
+			<th>Header2</th>
+			<th>Header3</th>
+		</tr>
+	    <tr>
+		    <td>1</td>
+			<td>2</td>
+			<td>3</td>
+		</tr>
+			<tr>
+		    <td>1</td>
+			<td>2</td>
+			<td>3</td>
+		</tr>
+		<tr>
+		    <td>1</td>
+			<td>2</td>
+			<td>3</td>
+		</tr>
+	</table>
+		
+
+	
+			
+			
---
+++ index1.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<style>
+    .table {
+		width: 350px;
+		border-collapse: collapse;
+	}
+	.row {
+		width: 100%;
+		display: flex;
+	}
+	.cell {
+		text-align: center;
+		width: 30%;
+		border: 1px solid black;
+	}
+	
+	</style>
+	
+<div id="main"class="table">
+	  <div class="row">
+	    <div class=cell>Header1</div>
+		<div class=cell>Header2</div>
+		<div class=cell>Header3</div>
+	</div>
+	<div class="row">
+	    <div class=cell>1</div>
+		<div class=cell>1</div>
+		<div class=cell>1</div>
+	</div>
+	<div class="row">
+	    <div class=cell>1</div>
+		<div class=cell>1</div>
+		<div class=cell>1</div>
+	</div>
+	<div class="row">
+	    <div class=cell>1</div>
+		<div class=cell>1</div>
+		<div class=cell>1</div>
+	</div>
