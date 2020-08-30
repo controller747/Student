ec398149a79052f18a39d73d6a6368a0ce4a807e
controller747 <70430521+controller747@users.noreply.github.com>
2020-08-30 23:52:12 +0300

    Update index2.php

--- index2.php
+++ index2.php
@@ -1,5 +1,9 @@
-<ul>
-<li><a href="">Кнопка</a></li>
-<li><a href="">Кнопка</a></li>
-<li><a href="">Кнопка</a></li>
+<style>
+  .menu li {display: inline;}
+</style>
+	
+<ul class="menu">
+  <li><a href="">Кнопка</a></li>
+  <li><a href="">Кнопка</a></li>
+  <li><a href="">Кнопка</a></li>
 </ul>
