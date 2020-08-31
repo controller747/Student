f6d4d29629b4e1a69ec61c36d3aec62a936a6173
controller747 <70430521+controller747@users.noreply.github.com>
2020-08-31 14:02:48 +0300

    Update index2.php

--- index2.php
+++ index2.php
@@ -1,10 +1,16 @@
 <style>
-  .menu li {display: inline;}
+	.menu li {display: inline;}
+	.menu a {
+		padding: 1em;
+		background: rgba(177, 152, 145, .3);
+		border-right: 1px solid #b19891;
+		color: #695753;}
+	.menu a:hover {background: #b19891;}		
 </style>
 	
 <ul class="menu">
-  <li><a href="">Кнопка</a></li>
-  <li><a href="">Кнопка</a></li>
-  <li><a href="">Кнопка</a></li>
-  <li><a href="">Кнопка</a></li>
+  <li><a href="">Button</a></li>
+  <li><a href="">Button</a></li>
+  <li><a href="">Button</a></li>
+  <li><a href="">Button</a></li>
 </ul>
