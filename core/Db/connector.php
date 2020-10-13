55ec2495fad861035c3062822662e56a69f447f0
controller747 <70430521+controller747@users.noreply.github.com>
2020-10-14 01:53:38 +0300

    homework

---
+++ core/Db/connector.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Core\Db\connector;
+
+class Connector
+{
+	protected $config;
+	
+	
+	public function __construct()
+	{
+		$this->setConfig();
+	}
+	 
+	public function setConfig():void
+	{
+		$this->config = include APP_ABSOLUTE_PATH . DIRECTORY_SEPARATOR . 'Config' . DIRECTORY_SEPARATOR . 'Db.php';
+	}
+	
+	 public function connectDB()
+    {
+//        if(is_array($this->config) && !empty($this->config)) {
+            var_export($this->config);
+            echo '<br>';
+            extract($this->config);
+            var_export($host);
+			$connect =  mysqli_connect($host, $user, $password);
+            var_export($connect);
+
+    }
+	
+}
---
+++ core/Db/insert.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Core\Database;
+
+class Insert
+{
+	public function query($query, $params = false) {
+    $success = $this->mysqli->query($this->getQuery($query, $params));
+    if ($success) {
+      if ($this->mysqli->insert_id === 0) return true;
+      else return $this->mysqli->insert_id;
+    }
+    else return false;
+  }
+}
